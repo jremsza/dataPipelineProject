9149841b46f5e56104e59da479405cfbd265621e
Jake Remsza <jremsza2@gmail.com>
2024-02-26 20:16:16 -0600

    First Commit

---
+++ pipelineExample/image_processing/image_processing.go
@@ -0,0 +1,67 @@
+package imageprocessing
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+// Error handling to ReadImage
+func ReadImage(path string) (image.Image, error) {
+	inputFile, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file %s: %v", path, err)
+	}
+	defer inputFile.Close()
+
+	// Decode the image
+	img, _, err := image.Decode(inputFile)
+	if err != nil {
+		fmt.Println(path)
+		panic(err)
+	}
+	return img, nil
+}
+
+// Added error handling to WriteImage
+func WriteImage(path string, img image.Image) error {
+	outputFile, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("error creating file: %w", err)
+	}
+	defer outputFile.Close()
+
+	// Encode the image to the new file
+	err = jpeg.Encode(outputFile, img, nil)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func Grayscale(img image.Image) image.Image {
+	// Create a new grayscale image
+	bounds := img.Bounds()
+	grayImg := image.NewGray(bounds)
+
+	// Convert each pixel to grayscale
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			originalPixel := img.At(x, y)
+			grayPixel := color.GrayModel.Convert(originalPixel)
+			grayImg.Set(x, y, grayPixel)
+		}
+	}
+	return grayImg
+}
+
+func Resize(img image.Image) image.Image {
+	newWidth := uint(500)
+	newHeight := uint(500)
+	resizedImg := resize.Resize(newWidth, newHeight, img, resize.Lanczos3)
+	return resizedImg
+}
---
+++ pipelineExample/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	imageprocessing "goroutines_pipeline/image_processing"
+	"image"
+	"strings"
+)
+
+type Job struct {
+	InputPath string
+	Image     image.Image
+	OutPath   string
+}
+
+func loadImage(paths []string) <-chan Job {
+	out := make(chan Job)
+	go func() {
+		// For each input path create a job and add it to
+		// the out channel
+		for _, p := range paths {
+			job := Job{InputPath: p,
+				OutPath: strings.Replace(p, "images/", "images/output/", 1)}
+			//added error handling
+			img, err := imageprocessing.ReadImage(p)
+			if err != nil {
+				fmt.Println("error reading image", err)
+				continue
+			}
+			job.Image = img
+			out <- job
+		}
+
+		close(out)
+	}()
+	return out
+}
+
+func resize(input <-chan Job) <-chan Job {
+	out := make(chan Job)
+	go func() {
+		// For each input job, create a new job after resize and add it to
+		// the out channel
+		for job := range input { // Read from the channel
+			job.Image = imageprocessing.Resize(job.Image)
+			out <- job
+		}
+		close(out)
+	}()
+	return out
+}
+
+func convertToGrayscale(input <-chan Job) <-chan Job {
+	out := make(chan Job)
+	go func() {
+		for job := range input { // Read from the channel
+			job.Image = imageprocessing.Grayscale(job.Image)
+			out <- job
+		}
+		close(out)
+	}()
+	return out
+}
+
+func saveImage(input <-chan Job) <-chan bool {
+	out := make(chan bool)
+	go func() {
+		for job := range input { // Read from the channel
+			err := imageprocessing.WriteImage(job.OutPath, job.Image)
+			if err != nil {
+				fmt.Println("error writing image", err)
+				out <- false
+				continue
+			}
+			out <- true
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+
+	imagePaths := []string{"images/ape1.jpg",
+		"images/ape2.jpg",
+		"images/ape3.jpg",
+		"images/ape4.jpg",
+	}
+
+	channel1 := loadImage(imagePaths)
+	channel2 := resize(channel1)
+	channel3 := convertToGrayscale(channel2)
+	writeResults := saveImage(channel3)
+
+	for success := range writeResults {
+		if success {
+			fmt.Println("Success!")
+		} else {
+			fmt.Println("Failed!")
+		}
+	}
+}
